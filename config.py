dd601edefb7a5c113c10f2cacb48a635c65d1567
phuctinh236-afk <phuctinh236@gmail.com>
2026-08-01 16:31:26 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,6 @@
+import os
 
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'mega_casino_world_vip_secret_key'
+    # Các cấu hình database (SQLite/PostgreSQL) sẽ thêm ở đây sau
+  

611de4d397447822d276ffa6a5349f8aa2d3502d
VietLOL869 <tranhoangquocviet1412@gmail.com>
2026-08-07 20:44:20 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,1 @@
-import os
 
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'mega_casino_world_vip_secret_key'
-    # Các cấu hình database (SQLite/PostgreSQL) sẽ thêm ở đây sau
-  
